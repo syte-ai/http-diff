42b237c31420c4240f1a91f82d0ff905f2552f4f
w00fmeow <w00fmeow@protonmail.ch>
2024-03-31 10:37:24 +0300

    - brew formula update

--- pkg/brew/http-diff.rb
+++ pkg/brew/http-diff.rb
@@ -1,14 +1,14 @@
-class HttpDiffBin < Formula
-  version '0.0.1'
+class HttpDiff < Formula
+  version '0.0.3'
   desc "CLI tool to verify consistency across web server versions."
   homepage "https://github.com/syte-ai/http-diff"
 
   if OS.mac?
       url "https://github.com/syte-ai/http-diff/releases/download/#{version}/http-diff-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "b0001ad91e9b1d759a2f23627deda5d0da4cf638e81046c9b999ecc009626059"
   elsif OS.linux?
       url "https://github.com/syte-ai/http-diff/releases/download/#{version}/http-diff-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "d03e506ea0cb15e4d96feabb55ae3c7521ade9b3a8c2a3dea7341d28765b7625"
   end
 
   conflicts_with "http-diff"
